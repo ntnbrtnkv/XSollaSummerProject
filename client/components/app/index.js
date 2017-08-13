da8c2865d5978538ffb69dc8d88800a72b1e3926
Anton <toha1100@gmail.com>
2017-08-13 18:00:41 +0500

    Fixed header back link.

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -3,7 +3,9 @@ export default {
         return (
             <div class="app">
                 <div class="page">
-                    <router-link class="page__back-link" to="/"><h1 class="page__title">Pharmacy</h1></router-link>
+                    <div class="page__title__container">
+                        <router-link class="page__back-link" to="/"><h1 class="page__title__text">Pharmacy</h1></router-link>
+                    </div>
                     <router-view></router-view>
                 </div>
             </div>
